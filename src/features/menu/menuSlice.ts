ca5bb840b9d8eebdac4ddf5371a86fb925205beb
Azamjon03042001 <azamjon03042001@gmail.com>
2025-04-27 19:53:09 +0500

    Исправлена маршрутизация и пути к картинкам для GitHub Pages

--- src/features/menu/menuSlice.ts
+++ src/features/menu/menuSlice.ts
@@ -1,6 +1,19 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { Pizza } from "./types";
 
+import bavarianImage from "../../assets/pizza-images/medium/bavarian.webp";
+import blueCheeseImage from "../../assets/pizza-images/medium/blue-cheese.webp";
+import carbonaraImage from "../../assets/pizza-images/medium/carbonara.webp";
+import cheesyImage from "../../assets/pizza-images/medium/cheesy.webp";
+import fourCheeseImage from "../../assets/pizza-images/medium/four-cheese.webp";
+import fourSeasonImage from "../../assets/pizza-images/medium/four-season.webp";
+import julienImage from "../../assets/pizza-images/medium/julien.webp";
+import margaritaImage from "../../assets/pizza-images/medium/margarita.webp";
+import meatMixImage from "../../assets/pizza-images/medium/meat-mix.webp";
+import meatImage from "../../assets/pizza-images/medium/meat.webp";
+import peperoniImage from "../../assets/pizza-images/medium/peperoni.webp";
+import pestoImage from "../../assets/pizza-images/medium/pesto.webp";
+
 interface MenuState {
   pizzas: Pizza[];
   loading: boolean;
@@ -21,7 +34,7 @@ const mockPizzas: Pizza[] = [
     description:
       "Увеличенная порция моцареллы, томаты, итальянские травы, фирменный томатный соус",
     price: 669,
-    image: "/src/assets/pizza-images/medium/margarita.webp",
+    image: margaritaImage, // Используем импортированную картинку
   },
   {
     id: 2,
@@ -29,7 +42,7 @@ const mockPizzas: Pizza[] = [
     description:
       "Пикантная пепперони, увеличенная порция моцареллы, томаты, фирменный томатный соус",
     price: 449,
-    image: "/src/assets/pizza-images/medium/peperoni.webp",
+    image: peperoniImage,
   },
   {
     id: 3,
@@ -37,14 +50,14 @@ const mockPizzas: Pizza[] = [
     description:
       "Баварские колбаски, маринованные огурчики, красный лук, томаты, горчичный соус, моцарелла, фирменный томатный соус",
     price: 789,
-    image: "/src/assets/pizza-images/medium/bavarian.webp",
+    image: bavarianImage,
   },
   {
     id: 4,
     name: "Креветки блю чиз",
     description: "Креветки, сыр блю чиз, моцарелла, фирменный соус альфредо",
     price: 649,
-    image: "/src/assets/pizza-images/medium/blue-cheese.webp",
+    image: blueCheeseImage,
   },
   {
     id: 5,
@@ -52,14 +65,14 @@ const mockPizzas: Pizza[] = [
     description:
       "Бекон, сыры чеддер и пармезан, моцарелла, томаты, красный лук, чеснок, фирменный соус альфредо, итальянские травы",
     price: 549,
-    image: "/src/assets/pizza-images/medium/carbonara.webp",
+    image: carbonaraImage,
   },
   {
     id: 6,
     name: "Сырная",
     description: "Моцарелла, сыры чеддер и пармезан, фирменный соус",
     price: 429,
-    image: "/src/assets/pizza-images/medium/cheesy.webp",
+    image: cheesyImage,
   },
   {
     id: 7,
@@ -67,7 +80,7 @@ const mockPizzas: Pizza[] = [
     description:
       "Увеличенная порция моцареллы, ветчина, пикантная пепперони, кубики брынзы, томаты, шампиньоны, итальянские травы, фирменный томатный соус",
     price: 449,
-    image: "/src/assets/pizza-images/medium/four-season.webp",
+    image: fourSeasonImage,
   },
   {
     id: 8,
@@ -75,7 +88,7 @@ const mockPizzas: Pizza[] = [
     description:
       "Цыпленок, шампиньоны, ароматный грибной соус, лук, сухой чеснок, моцарелла, смесь сыров чеддер и пармезан, фирменный соус альфредо",
     price: 499,
-    image: "/src/assets/pizza-images/medium/julien.webp",
+    image: julienImage,
   },
   {
     id: 9,
@@ -83,7 +96,7 @@ const mockPizzas: Pizza[] = [
     description:
       "Сыр блю чиз, сыры чеддер и пармезан, моцарелла, фирменный соус альфредо",
     price: 489,
-    image: "/src/assets/pizza-images/medium/four-cheese.webp",
+    image: fourCheeseImage,
   },
   {
     id: 10,
@@ -91,7 +104,7 @@ const mockPizzas: Pizza[] = [
     description:
       "Пряная говядина, баварские колбаски, пикантная пепперони, бекон, моцарелла и фирменный томатный соус",
     price: 489,
-    image: "/src/assets/pizza-images/medium/meat-mix.webp",
+    image: meatMixImage,
   },
   {
     id: 11,
@@ -99,7 +112,7 @@ const mockPizzas: Pizza[] = [
     description:
       "Цыпленок, соус песто, кубики брынзы, томаты, моцарелла, фирменный соус альфредо",
     price: 499,
-    image: "/src/assets/pizza-images/medium/pesto.webp",
+    image: pestoImage,
   },
   {
     id: 12,
@@ -107,7 +120,7 @@ const mockPizzas: Pizza[] = [
     description:
       "Цыпленок, ветчина, пикантная пепперони, острые колбаски чоризо, моцарелла, фирменный томатный соус",
     price: 499,
-    image: "/src/assets/pizza-images/medium/meat.webp",
+    image: meatImage,
   },
 ];
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.tsx";
+import { persistor, store } from "./store/index.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </PersistGate>
+    </Provider>
+  </StrictMode>
 );
